fc7ae0671940fc8e9cf62aea64ffc3a3fb6c525b
hamzashoukat94 <hamza94shoukat@gmail.com>
2023-08-16 09:33:14 +0500

    Movie App

---
+++ PracticeWebApp/Data/PracticeWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PracticeWebApp.Models;
+
+namespace PracticeWebApp.Data
+{
+    public class PracticeWebAppContext : DbContext
+    {
+        public PracticeWebAppContext (DbContextOptions<PracticeWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PracticeWebApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ PracticeWebApp/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PracticeWebApp.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PracticeWebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PracticeWebAppContext") ?? throw new InvalidOperationException("Connection string 'PracticeWebAppContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
